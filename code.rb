d8b48906ddf7e92552d2a33ca6208687d8b9c3b5
Padrig Tuck <Padrig@gmail.com>
2016-05-27 10:13:55 -0400

    assignment finished

---
+++ code.rb
@@ -0,0 +1,69 @@
+# Run the code. Change it so that it looks like the required output.
+
+require 'erb'
+
+recipe = {
+  name: "Roasted Brussels Sprouts",
+  ingredients: [
+    "1 1/2 pounds Brussels sprouts",
+    "3 tablespoons good olive oil",
+    "3/4 teaspoon kosher salt",
+    "1/2 teaspoon freshly ground black pepper"
+  ],
+  directions: [
+    "Preheat oven to 400 degrees F.",
+    "Cut off the brown ends of the Brussels sprouts.",
+    "Pull off any yellow outer leaves.",
+    "Mix them in a bowl with the olive oil, salt and pepper.",
+    "Pour them on a sheet pan and roast for 35 to 40 minutes.",
+    "They should be until crisp on the outside and tender on the inside.",
+    "Shake the pan from time to time to brown the sprouts evenly.",
+    "Sprinkle with more kosher salt ( I like these salty like French fries).",
+    "Serve and enjoy!"
+  ]
+}
+
+recipe_title = "Recipe: #{recipe[:name]}"
+recipe_directions = ""
+recipe_ingredients = ""
+i = 0
+
+puts "#===================================#"
+puts "# #{recipe_title}  #"
+puts "#===================================#"
+puts
+puts "Ingredients"
+puts "-----------"
+puts
+recipe[:ingredients].each do |ingredient|
+  puts "#{ingredient}"
+end
+puts
+recipe[:directions].each do |direction|
+  puts "#{i+=1}. #{direction}"
+end
+
+puts
+puts
+
+recipe_template = <<-ERB
+
+#=<%= "=" * recipe_title.length %>=#
+# <%= recipe_title %> #
+#=<%= "=" * recipe_title.length %>=#
+
+    Ingredients
+    -----------
+
+  <% recipe[:ingredients].each do |ingredient| %>
+    <%= ingredient %>
+    <% end %>
+
+  <% recipe[:directions].each_with_index do |direction, num| %>
+    <%= num + 1 %>. <%= direction %>
+    <% end %>
+
+ERB
+
+erb = ERB.new(recipe_template)
+puts erb.result
